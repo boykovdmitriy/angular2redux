c86cfe45101bdb99d6d33a477000ccef419f05be
Boykov Dmitriy <boikovdmitriy@mail.ru>
2017-03-28 23:28:44 +0300

    added webpack-dev-middleware

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /**
  * Created by Boikov on 3/14/2017.
  */
-const express = require('express');
-const app     = express();
-const router  = express.Router();
-var counter   = 0;
+const express           = require('express');
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpack           = require('webpack');
+const config            = require('./webpack.config');
+const app               = express();
+const router            = express.Router();
+const compiler          = webpack(config);
+var counter             = 0;
 router.get('/api/counter', function (req, res) {
 	res.status(200).json({
 		data: counter
@@ -24,7 +28,7 @@ router.post('/api/decrement', function (req, res) {
 		data: counter
 	});
 });
-
+app.use(webpackMiddleware(compiler));
 app.use(router);
 app.use(express.static('public'));
 
